d7b1940ceab9b25b37ce15e6806c05e44a92624f
V. Butko <butkovv1991@gmail.com>
2020-02-19 15:51:30 +0300

    add fixture, update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,13 +1,13 @@
 import path from 'path';
-// import { readFileSync } from 'fs';
 import genDiff from '../src/gendiff';
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-
-const path1 = getFixturePath('before.json');
-const path2 = getFixturePath('after.json');
-
 test('main flow', () => {
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const path1 = getFixturePath('before.json');
+  const path2 = getFixturePath('after.json');
+  const path3 = getFixturePath('empty.json');
   expect(genDiff(path1, path2)).toEqual('{\n  host: hexlet.io\n+ timeout: 20\n- timeout: 50\n- proxy: 123.234.53.22\n- follow: false\n+ verbose: true\n}');
+  expect(genDiff(path1, path3)).toEqual('{\n- host: hexlet.io\n- timeout: 50\n- proxy: 123.234.53.22\n- follow: false\n}');
+  expect(genDiff(path1, path1)).toEqual('{\n  host: hexlet.io\n  timeout: 50\n  proxy: 123.234.53.22\n  follow: false\n}');
+  expect(genDiff(path3, path3)).toEqual('{\n\n}');
 });
